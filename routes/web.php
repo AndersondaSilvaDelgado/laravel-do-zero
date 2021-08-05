bc25e08ae9316ff718a4a888d649b1f230ce7d47
anderson.sdelgado@gmail.com <anderson.sdelgado@gmail.com>
2021-08-05 16:49:32 -0300

    Criando todas as Rotas, Controller e View do Site

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Http\Controllers\Site\{
+    AboutController,
+    HomeController,
+    CategoryController,
+    BlogController,
+    ContactController
+};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class)->name('site.index');
+Route::get('/produtos', [CategoryController::class, 'index'])->name('site.produtos.index');
+Route::get('/produtos/{slug}', [CategoryController::class, 'show'])->name('site.produtos.show');
+Route::get('/blog', BlogController::class)->name('site.blog');
+Route::get('/sobre', AboutController::class)->name('site.about');
+Route::get('/contatos', [ContactController::class, 'index'])->name('site.contato.index');
+Route::post('/contatos', [ContactController::class, 'msg'])->name('site.contato.msg');
